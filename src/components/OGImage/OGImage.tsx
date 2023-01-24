fe63924e40dd1671c61a85ecb59797f9d21259ba
JumboWumbus <stepabovetheeggs@protonmail.com>
2023-01-24 20:37:18 +0000

    maybe done

--- src/components/OGImage/OGImage.tsx
+++ src/components/OGImage/OGImage.tsx
@@ -2,7 +2,6 @@
 import { convertTime } from 'src/utils';
 
 
-import s from "styles/test.module.scss";
 
 // const style = `
 //   .font-logo {
@@ -53,6 +52,10 @@ import s from "styles/test.module.scss";
 
 //TODO inline style pseudoelement
 
+
+
+
+
 const container = {
    position: 'relative',
    paddingInline: '4rem',
@@ -85,14 +88,7 @@ const gridStyle = {
 
 const fade = {
 
-   content: '',
-   position: 'absolute',
-   zIndex: -1,
-   top: 0,
-   left: 0,
-   bottom: 0,
-   right: 0,
-   background: 'radial-gradient(transparent 10%, #ffffff 90%)'
+
 
 };
 
@@ -138,15 +134,64 @@ export const OGImage = ({
 }) => (
 
    <>
-      <div style={container}>
-         <div style={gridStyle}></div>
-         <div style={fade}></div>
-         <div style={postInfoContainer}>
-            <h1 style={postTitle}>{title}</h1>
+
+      <div style={{
+         position: 'relative',
+         paddingInline: '4rem',
+
+         //REMOVE WHEN MOVING TO REAL THING
+         width: '1200px',
+         height: '630px',
+      }}>
+         <div style={{
+            position: 'absolute',
+            zIndex: -2,
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            backgroundImage: `url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 101 101"><path d="M1 10.5H0V0h10.5v1H1zM75.3 1H58.1V0h17.2v1zM42.9 1H25.7V0h17.2v1zm58.1 9.5h-1V1h-9.5V0H101zm0 64.8h-1V58.1h1v17.2zm0-32.4h-1V25.7h1v17.2zm0 58.1H90.5v-1h9.5v-9.5h1zm-25.7 0H58.1v-1h17.2v1zm-32.4 0H25.7v-1h17.2v1zm-32.4 0H0V90.5h1v9.5h9.5zM1 75.3H0V58.1h1v17.2zm0-32.4H0V25.7h1v17.2z"/></svg>')`,
+            backgroundPosition: 'center',
+            //MAY change later if contrast bad
+            opacity: '60%',
+         }}></div>
+         <div style={{
+            content: '',
+            position: 'absolute',
+            zIndex: -1,
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            background: 'radial-gradient(transparent 10%, #ffffff 90%)'
+         }}></div>
+         <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100%',
+            height: '100%',
+            alignItems: 'flex-start',
+            justifyContent: 'center'
+         }}>
+            <h1 style={{
+               fontSize: '5rem',
+               fontWeight: 700,
+            }}>{title}</h1>
             <p>{excerpt}</p>
          </div>
 
-         <div style={footer}>
+         <div style={{
+            position: 'absolute',
+            bottom: 0,
+            width: '100%',
+            height: '100px',
+            display: 'flex',
+            justifyContent: 'space-between',
+            fontSize: '2rem',
+            textTransform: 'uppercase',
+            fontWeight: 700,
+            alignItems: 'center',
+         }}>
             <p>{date}</p>
             <p>{readMinutes}</p>
          </div>
