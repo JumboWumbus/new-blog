f8651ada02366a811fb8e2c1a3b95a2355c00237
JumboWumbus <stepabovetheeggs@protonmail.com>
2023-02-01 12:32:09 +0000

    under construction

--- notes/grumbly.md
+++
@@ -1,6 +0,0 @@
----
-name: greng
-kill: please
----
-
-OH MY GOD PLEASE WORK I'VE SPENT TOO LONG ON THIS FOR IT TO FAIL >:(
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,124 +14,32 @@ const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
 
   const previewImage = {
-		url: `${mainUrl}/api/og?${objToUrlParams({
-		  header: `BensDen ⟶ My website`,
-		  title: `Developing, Designing, screaming...\n                          all at a high level`,
-		  subtitle: `The most mediocre site in the world.`
-		})}`,
-		description: `Personal website of Ben Hammond`
-	 }
+    url: `${mainUrl}/api/og?${objToUrlParams({
+      header: `BensDen ⟶ My website`,
+      title: `Developing, Designing, screaming...\n                          all at a high level`,
+      subtitle: `The most mediocre site in the world.`
+    })}`,
+    description: `Personal website of Ben Hammond`
+  }
 
   return (
     <>
 
-    
+
       <SEO
-          title={`BensDen | The worst place to be!`} description={`This is my personal website where I put all of my ideas, work and sweat.`} slug={``} previewImage={previewImage}
-        />
+        title={`BensDen | The worst place to be!`} description={`This is my personal website where I put all of my ideas, work and sweat.`} slug={``} previewImage={previewImage}
+      />
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              By{' '}
-              <Image
-                src='/vercel.svg'
-                alt='Vercel Logo'
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+        <h1>THE MIGHTY EYE OBSERVES AND LOOKS UPON YOU WITHOUT JUDGMENT... HOWEVER THE BEAST ALSO LOOKS AT YE WITHOUT PITY. ALL WHO COME TO MEET THE BEAST KNOW NOT OF THEIR FUTURE, BUT ONLY OF THEIR PAST TRANSGRESSIONS.</h1>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src='/next.svg'
-            alt='Next.js Logo'
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src='/thirteen.svg'
-              alt='13'
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
+        <img
 
-        <div className={styles.grid}>
-          <a
-            href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
+          src="/webImages/Me.webp"
+          alt="The pretty creator"
+        />
 
-          <a
-            href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
+        <h2>Site is under construction, pls await further cool features xoxo</h2>
 
-          <a
-            href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
       </main>
     </>
   );
