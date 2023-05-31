bdf679055a7b08162f5451a6284c0e443027dea9
JumboWumbus <stepabovetheeggs@protonmail.com>
2023-05-31 14:38:15 +0100

    changing fetch test

--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -33,6 +33,39 @@ export const getAllPosts = () => {
 	return posts;
 };
 
+export const getAllPostMetadata = ()=>{
+	const posts = getSlugs().map((slug) => getPostMetadataFromSlug(slug)).sort((a,b) =>{
+		if(a.date > b.date)return 1;
+		if(a.date < b.date) return -1;
+		return 0;
+	})
+	.reverse();
+	return posts;
+}
+
+export const getPostMetadataFromSlug = (slug: string): PostMeta => {
+	const postPath = path.join(ARTICLES_DIRECTORY, `${slug}.mdx`);
+	const source = fs.readFileSync(postPath);
+  
+	const { data, content } = matter(source);
+  
+	const postStats = readingTime(content);
+	const minutes = Math.round(postStats.minutes / 5) * 5;
+  
+	return {
+	  slug,
+	  excerpt: data.excerpt ?? "",
+	  summary: data.summary ?? "",
+	  title: data.title ?? slug,
+	  author: data.author ?? "Ben Hammond",
+	  tags: (data.tags ?? []).sort(),
+	  category: data.category ?? "needs category",
+	  date: (data.date ?? new Date()).toString(),
+	  imageURL: data.imageURL ?? "ArticleImages/default.webp",
+	  readingTime: `${minutes} min read`,
+	};
+  };
+
 export const getPostFromSlug = (slug: string): Post => {
 	const postPath = path.join(ARTICLES_DIRECTORY, `${slug}.mdx`);
 	const source = fs.readFileSync(postPath);
