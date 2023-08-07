ccc8f788a05f6a7fb56369c4eed223001a7a2dd5
Gremblo <stepabovetheeggs@protonmail.com>
2023-08-08 00:25:44 +0100

    fixed view issue

--- src/lib/supabase-admin.ts
+++ src/lib/supabase-admin.ts
@@ -3,6 +3,13 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl: string = process.env.SUPABASE_URL || '';
 const supabaseApi: string = process.env.SUPABASE_API || '';
 
+const isValidSupabaseUrl = supabaseUrl.trim().length > 0;
+const isValidSupabaseApiKey = supabaseApi.trim().length > 0;
+
+if (!isValidSupabaseUrl || !isValidSupabaseApiKey) {
+  throw new Error('Please provide valid Supabase URL and API key.');
+}
+
 const SupabaseAdmin = createClient(supabaseUrl, supabaseApi);
 
 export { SupabaseAdmin };
